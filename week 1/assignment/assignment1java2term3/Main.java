7e1623939468eac13e46e1fcd1dbaf7094b64c43
Bright Tenalo <debrightess@yahoo.com>
2022-09-05 22:06:25 +0100

    complete week 1

---
+++ week 1/assignment/assignment1java2term3/Main.java
@@ -0,0 +1,7 @@
+package assignment1java2term3;
+public class Main {
+    public static void main(String[] args) {
+
+        BenchmarkingSortingAlgorithms SortingTest= new BenchmarkingSortingAlgorithms();
+    }
+}
